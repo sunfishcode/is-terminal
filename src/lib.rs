91ed9acd8610e2ed4fe9072727e71f1366fcd15b
Sergio Benitez <sb@sergio.bz>
2023-03-23 14:35:48 -0700

    Add 'is_terminal' function for importless checking.

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,7 @@ use std::os::windows::io::AsRawHandle;
 #[cfg(windows)]
 use windows_sys::Win32::Foundation::HANDLE;
 
+/// Extension trait to check whether something is a terminal.
 pub trait IsTerminal {
     /// Returns true if this is a terminal.
     ///
@@ -51,6 +52,19 @@ pub trait IsTerminal {
     /// }
     /// ```
     fn is_terminal(&self) -> bool;
+}
+
+/// Returns `true` if `this` is a terminal.
+///
+/// # Example
+///
+/// ```
+/// if is_terminal::is_terminal(&std::io::stdout()) {
+///     println!("stdout is a terminal")
+/// }
+/// ```
+pub fn is_terminal<T: IsTerminal>(this: &T) -> bool {
+    this.is_terminal()
 }
 
 #[cfg(not(target_os = "unknown"))]
